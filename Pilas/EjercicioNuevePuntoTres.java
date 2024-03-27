b0a0f1d78eae5c31bc646fa53d7508576ae3fe94
Jhon Cano <johnhcano@gmail.com>
2024-03-27 15:20:37 -0500

    Punto 9.3

--- Pilas/EjercicioNuevePuntoTres.java
+++ Pilas/EjercicioNuevePuntoTres.java
@@ -4,10 +4,9 @@ public class EjercicioNuevePuntoTres {
 
     /*
      * Escribir un programa que, haciendo uso de una Pila, procese cada uno de los
-     * caracteres de una
-     * expresión que viene dada en una línea. La finalidad es verificar el
-     * equilibrio de paréntesis,
-     * llaves y corchetes. Por ejemplo, la siguiente expresión tiene un número de
+     * caracteres de una expresión que viene dada en una línea. 
+     * La finalidad es verificar el equilibrio de paréntesis, llaves y corchetes. 
+     * Por ejemplo, la siguiente expresión tiene un número de
      * paréntesis equilibrado:
      * ((a+b)*5) - 7
      */
