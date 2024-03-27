ef59614f8b16164f1009aff7235776bcfee03ab2
Jhon Cano <johnhcano@gmail.com>
2024-03-26 20:39:10 -0500

    Se agrega descripción del problema

--- Pilas/Ejemplo1.java
+++ Pilas/Ejemplo1.java
@@ -1,6 +1,13 @@
 import java.util.Stack;
 
 public class Ejemplo1 {
+
+    /*
+      Escribir un programa que, haciendo uso de una Pila, procese cada uno de los caracteres de una 
+      expresión que viene dada en una línea. La finalidad es verificar el equilibrio de paréntesis, 
+      llaves y corchetes. Por ejemplo, la siguiente expresión tiene un número de paréntesis equilibrado:
+      ((a+b)*5) - 7 
+    */
  
     public static void main(String[] args) {
  
