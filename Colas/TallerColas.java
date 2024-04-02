da7037e63ff8f0c4704c5d91fc4455c463b75435
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 16:03:04 +0000

    Taller de Colas en JAVA QUEUE

--- Colas/TallerColas.java
+++ Colas/TallerColas.java
@@ -1,7 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class TallerColas{
     public static void main(String[] args) {
         
-        System.out.println("QUEUE");
+        Queue<String> cola = new ArrayDeque<>();
+
+        cola.add("Messi");
+        cola.add("Cristiano");
+        cola.add("Mbapee");
+        cola.add("James");
+
+        System.out.println("Elementos dentro de la Cola: " + cola);
+
+
+
 
 
 
