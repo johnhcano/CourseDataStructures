39f67268de5992ab1195a1aef8e6f2da2fa00103
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 00:49:18 +0000

    Ejercicio de Colas

--- Colas/EjemploColas.java
+++ Colas/EjemploColas.java
@@ -22,6 +22,24 @@ public class EjemploColas {
         //Mostrar la cabeza de la cola sin remover el elemento.
         System.out.println("Cabeza de la Cola: " + cola.element());
 
-        
+        //Agregar un elemento al final de la cola con el método (offer)
+        cola.offer(20);
+
+        //Mostramos los elementos de la cola.
+        System.out.println("Elementos de la cola: " + cola);
+
+        //Mostrar la cabeza de la cola sin remover el elemento de la cola 
+        //y en caso de que la cola este vacía devolvera "null"
+        System.out.println(cola.peek());
+
+        //Remover la cabeza de la cola y en caso de que la cola este vacía devolvera "null"
+        System.out.println(cola.poll());
+
+        //Remover la cabeza de la cola
+        System.out.println(cola.remove());
+
+        //Mostramos los elementos de la cola.
+        System.out.println("Elementos de la cola: " + cola);
+
     }
 }
