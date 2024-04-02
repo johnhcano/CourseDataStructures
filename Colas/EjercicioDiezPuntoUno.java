9eec9e17886e2db3e17a564a2be946becbe18fe2
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 01:29:17 +0000

    Ejercicio DiezPuntoUno

--- Colas/EjercicioDiezPuntoUno.java
+++ Colas/EjercicioDiezPuntoUno.java
@@ -1,5 +1,8 @@
 package Colas;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class EjercicioDiezPuntoUno {
     public static void main(String[] args) {
         /*
@@ -9,8 +12,23 @@ public class EjercicioDiezPuntoUno {
             los números negativos que tiene la cola original.
          */
 
-         int n = (int) (Math.floor(Math.random()*((-25)-25+1)+25));
-         System.out.println(n);
+         int n;
+
+         Queue<Integer> colaAleatorios = new ArrayDeque<>();
+         Queue<Integer> colaNegativos = new ArrayDeque<>();
+
+         for(int i = 1; i <= 10; i++){
+            n = (int) (Math.floor(Math.random()*((-25)-25+1)+25));
+            colaAleatorios.add(n);
+            if (n < 0){
+                colaNegativos.add(n);
+            }
+         }
+
+         System.out.println(colaAleatorios);
+         System.out.println(colaNegativos);
+
+
 
     }
 }
