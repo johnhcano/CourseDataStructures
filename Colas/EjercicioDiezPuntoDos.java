c8507eb34c59c92232871b3cc3b555ec99c0b815
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 16:28:59 +0000

    Ejercicio DiezPuntoDos del Libro

---
+++ Colas/EjercicioDiezPuntoDos.java
@@ -0,0 +1,17 @@
+import java.util.Queue;
+
+public class EjercicioDiezPuntoDos {
+    public static void main(String[] args) {
+        /*
+        * Escribir un método que tenga como argumentos dos colas del mismo tipo 
+        * y devuelva cierto si las dos colas son idénticas.
+        */
+    }
+
+    public boolean validarColasIdenticas(Queue<Integer> c1, Queue<Integer> c2){
+        boolean flag = false;
+        return flag;
+    }
+
+
+}
