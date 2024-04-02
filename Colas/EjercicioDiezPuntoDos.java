21d49820b477625331471841b2f8aeff9432529c
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 16:43:13 +0000

    Ejercicio DiezPuntoDos del Libro

--- Colas/EjercicioDiezPuntoDos.java
+++ Colas/EjercicioDiezPuntoDos.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class EjercicioDiezPuntoDos {
@@ -10,6 +11,19 @@ public class EjercicioDiezPuntoDos {
 
     public boolean validarColasIdenticas(Queue<Integer> c1, Queue<Integer> c2){
         boolean flag = false;
+        int cabezaCola1 = 0;
+        int cabezaCola2 = 0;
+        for (int i = 1; i <= c1.size(); i++) {
+            cabezaCola1 = c1.remove();
+            cabezaCola2 = c2.remove();
+            if(cabezaCola1 == cabezaCola2){
+                flag = true;
+            }else{
+                flag = false;
+                break; //System.exit(0);
+            }
+        }
+
         return flag;
     }
 
