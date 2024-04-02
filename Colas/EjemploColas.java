6ee0706a5593bfd01a947d12743062a724d13221
JhonHCanoB <johnhcano@gmail.com>
2024-04-02 00:31:11 +0000

    Ejercicio de Colas

--- Colas/EjemploColas.java
+++ Colas/EjemploColas.java
@@ -1,7 +1,21 @@
 package Colas;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 public class EjemploColas {
     public static void main(String[] args) {
         
+        Queue<Integer> cola = new ArrayDeque<>();
+
+        cola.add(10);
+        cola.add(5);
+        cola.add(8);
+        cola.add(16);
+        cola.add(6);
+
+        System.out.println("Elementos de la cola: " + cola);
+
     }
 }
