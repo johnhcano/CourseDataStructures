6aec7acda8946ad4453e85a5acd836c5359a789e
JhonHCanoB <johnhcano@gmail.com>
2024-05-07 21:04:37 +0000

    Se agregó rawtypes

--- Maps/MapEnJava.java
+++ Maps/MapEnJava.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class MapEnJava {
+    @SuppressWarnings("rawtypes")
     public static void main(String[] args) {
         Map<Integer, String> estudiantes = new HashMap<>();
 
