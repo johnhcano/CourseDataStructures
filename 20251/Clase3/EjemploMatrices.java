f280febaead4ac910ba5552b87c465af85727868
JhonHCanoB <johnhcano@gmail.com>
2025-02-18 17:15:45 +0000

    Matrices

---
+++ 20251/Clase3/EjemploMatrices.java
@@ -0,0 +1,16 @@
+public class EjemploMatrices {
+    public static void main(String[] args) {
+
+        // Declaración de una matriz de 3x3
+        int[][] m = { { 9, 3, 5 },
+                      { 1, 4, 2 },
+                      { 8, 7, 6 } };
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                System.out.println("m[" + i + "][" + j + "]=" + m[i][j]);
+            }
+
+        }
+    }
+}
