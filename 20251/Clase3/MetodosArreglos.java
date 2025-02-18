7601d9304dd70641c25babc93c74e465dc3bb5e0
JhonHCanoB <johnhcano@gmail.com>
2025-02-18 16:04:37 +0000

    Creación de la clase MetodosArreglos

---
+++ 20251/Clase3/MetodosArreglos.java
@@ -0,0 +1,27 @@
+public class MetodosArreglos {
+
+    public String nombresSueldos(String[] n, int[] s){
+        int suma = 0;
+        //Calcular la suma de los sueldos de los empleados
+        for (int i = 0; i < s.length; i++) {
+            suma += s[i];
+        }
+        
+        //Calcular el promedio de los sueldos
+        double promedio = suma / s.length;
+
+        String cad = "";
+        int contador = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] >= promedio) {
+                cad += n[i] + "\n";
+                contador++;
+            }
+        }
+
+        cad += "Fueron " + contador + " los empleados que tiene sueldo por encima del promedio: " +promedio;
+
+        return cad;
+    }
+}
