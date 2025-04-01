bf06e1d90da51b96559d1c97a0cde784242f1c89
JhonHCanoB <johnhcano@gmail.com>
2025-04-01 16:26:27 +0000

    Creación de los métodos get, toString y pagar

--- 20251/Clase9/Trabajador.java
+++ 20251/Clase9/Trabajador.java
@@ -2,13 +2,32 @@ public class Trabajador {
 
     private String cedula;
     private String nombre;
-    private double sueldo;
+    private double salario;
     
-    public Trabajador(String cedula, String nombre, double sueldo){
+    public Trabajador(String cedula, String nombre, double salario){
         this.cedula = cedula;
         this.nombre = nombre;
-        this.sueldo = sueldo;
+        this.salario = salario;
     }
 
+    public String getCedula(){
+        return cedula;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public String toString(){
+        return "Trabajador { Cedula: " + cedula + " Nombre: " + nombre + " Salario: " + salario + "}"; 
+    }
+
+    public double pagar(double incremento){
+        return salario + (salario * incremento);
+    }
 
 }
