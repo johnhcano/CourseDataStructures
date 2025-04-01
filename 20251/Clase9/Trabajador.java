af77641c16bdef4d29a2b57cad2b434da1436dfa
JhonHCanoB <johnhcano@gmail.com>
2025-04-01 16:12:07 +0000

    Creación de la carpeta clase9 y la clase Trabajador

---
+++ 20251/Clase9/Trabajador.java
@@ -0,0 +1,14 @@
+public class Trabajador {
+
+    private String cedula;
+    private String nombre;
+    private double salario;
+    
+    public Trabajador(String cedula, String nombre, double salario){
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    
+}
