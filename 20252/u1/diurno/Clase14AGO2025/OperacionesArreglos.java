ad6f7b68ce043bbd1a9712a712a3903ef48ad622
JhonHCanoB <johnhcano@gmail.com>
2025-08-04 14:13:47 +0000

    Método llenarArregloEnteros

--- 20252/u1/diurno/Clase14AGO2025/OperacionesArreglos.java
+++ 20252/u1/diurno/Clase14AGO2025/OperacionesArreglos.java
@@ -1,4 +1,18 @@
+import java.util.Random;
 
 public class OperacionesArreglos {
     
+    //Método para llenar un arreglo de enteros
+    public int[] llenarArregloEnteros(int tam){
+
+        Random rand = new Random();
+
+        int[] numeros = new int[tam];
+
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] = rand.nextInt(100 - 1 ) + 1;            
+        }
+
+        return numeros;
+    }
 }
