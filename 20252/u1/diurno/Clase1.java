74e6735ec44269c85e04dde90b53cf7e1efe550b
JhonHCanoB <johnhcano@gmail.com>
2025-08-04 14:05:01 +0000

    creación archivo OperacionesArreglos

--- 20252/u1/diurno/Clase1.java
+++
@@ -1,7 +0,0 @@
-public class Clase1 {
-    public static void main(String[] args){
-
-        System.out.println("Estructura de Datos");
-
-    }
-}
---
+++ 20252/u1/diurno/Clase14AGO2025/Clase1.java
@@ -0,0 +1,20 @@
+package Clase14AGO2025;
+public class Clase1 {
+    public static void main(String[] args){
+
+        System.out.println("Estructura de Datos");
+
+        //Arreglos ó Arrays ó Vectores
+
+        //Declaración del arreglo. Forma 1
+        //int[] numeros;
+        //numeros = new int[5];
+
+        //Declaración del arreglo. Forma 2
+        //int[] numeros = new int[5];
+
+        //Declaración e inicialización
+        int[] numeros = {10, 20, 30, 40, 50};
+
+    }
+}
